fc455b94ddfe4916fea2481b8d48222960b3fa65
Hide <hshidara@ucdavis.edu>
2020-05-07 16:39:28 -0700

    Updates

---
+++ precache-manifest.a49f2e137994a393491336f22427293a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecca306096270a7c31de99a97a909c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1ab239ed04ab342fbf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "10c9e9cc8b81af352638",
+    "url": "/static/js/2.16bb7895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16bb7895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1ab239ed04ab342fbf",
+    "url": "/static/js/main.878dda9d.chunk.js"
+  },
+  {
+    "revision": "de2705d02b12182bbac4",
+    "url": "/static/js/runtime-main.331dc322.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
